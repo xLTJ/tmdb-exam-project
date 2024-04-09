1d1f4c5710b0f4dfbdc4f5f409403a138a7b2b90
xLTJ <lxcy.3@proton.me>
2024-04-09 14:07:46 +0200

    Changed margin of Titles

--- src/components/LandingPageComponents/LandingPage.jsx
+++ src/components/LandingPageComponents/LandingPage.jsx
@@ -8,11 +8,11 @@ export default function LandingPage() {
             <Hero/>
             <main className={'py-10'}>
                 <div className={"container mb-10"}>
-                    <h3 className={"text-3xl font-bold"}>Popular</h3>
+                    <h3 className={"text-3xl font-bold ml-6"}>Popular</h3>
                     <MovieCarouselDisplay movieList={testMovieList.popular}/>
                 </div>
                 <div className={"container mb-10"}>
-                    <h3 className={"text-3xl font-bold"}>Top Rated</h3>
+                    <h3 className={"text-3xl font-bold ml-6"}>Top Rated</h3>
                     {/*/discover/movie?sort_by=popularity.desc&page=1&vote_average.gte=8.0*/}
                     <MovieCarouselDisplay movieList={testMovieList.top_rated}/>
                 </div>
