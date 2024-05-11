da72e5be35a928bb9cda687a9b078f121dc1c7b0
xLTJ <lxcy.3@proton.me>
2024-05-11 17:55:19 +0200

    MOREEEE

--- src/components/GraphPage/Graph.jsx
+++ src/components/GraphPage/Graph.jsx
@@ -31,8 +31,8 @@ export default function Graph({graphData}) {
     // Add bloom effect to the graph
     useEffect(() => {
         const bloomPass = new UnrealBloomPass();
-        bloomPass.strength = 3;
-        bloomPass.radius = 2;
+        bloomPass.strength = 5;
+        bloomPass.radius = 1.5;
         bloomPass.threshold = 0;
 
         const composer = graphRef.current.postProcessingComposer()
