4db5f9a6ca1b7019f2b984c0f54e86680ddbf22c
StagnantBytes <artyom.shevchenko2005@gmail.com>
2024-05-13 08:59:06 +0200

    Tilført søgning til vores hjemmeside gennem multi search metoden da discover ikke tage navne. næste del er fremvisning af film data

--- src/services/tmdbApi.js
+++ src/services/tmdbApi.js
@@ -1,5 +1,26 @@
 const APIKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNTY2M2Y4YjI3YWQwYzQyMjE4MmMyM2M4YmU4OTM5YSIsInN1YiI6IjY1YzMzMDJiYjc2Y2JiMDE2YjBmY2UxYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6AvmVDtqBLzx4mB1LmZyEA7D9LEa6MWY-bbmnGea2U0";
 const TmdbApi = {
+    // Calls the TMDB API to perform a multi search
+    async multiSearch(query) {
+        const searchParams = new URLSearchParams({
+            query: query,
+            include_adult: false,
+            language: 'en-US',
+            page: 1
+        }).toString();
+
+        const payload = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${APIKey}`
+            },
+        }
+
+        const body = await fetch(`https://api.themoviedb.org/3/search/multi?${searchParams}`, payload)
+        const response = await body.json();
+        return response;
+    },
     // Calls the TMDB API to discover movies
     async discoverMovies(options) {
         const searchParams = new URLSearchParams(options).toString();
