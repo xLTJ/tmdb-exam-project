7e7bf60f4f6b714d758af11cd643c6aa4676e1bd
StagnantBytes <artyom.shevchenko2005@gmail.com>
2024-05-14 09:48:58 +0200

    Able to add to movies to graph, still need to work out a bit og bugs such as the name of the added movie not shoving in the graph page. IDK why

--- src/components/InfoComponents/MovieInfo.jsx
+++ src/components/InfoComponents/MovieInfo.jsx
@@ -1,10 +1,12 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import tmdbApi from "../../services/tmdbApi";
+import useAddToGraph from "../../services/AddToGraph";
 
 export default function MovieInfo() {
     const {mediaType, movieId} = useParams();
     const [details, setDetails] = useState(null);
+    const addToGraph = useAddToGraph();
 
     useEffect(() => {
         const fetchDetails = async () => {
@@ -12,7 +14,7 @@ export default function MovieInfo() {
             try {
                 if (mediaType === 'movie') {
                     const movieDetails = await tmdbApi.getMovieDetails(movieId);
-                    console.log(`Movie Details: ${movieDetails}`)
+                    console.log('Movie Details:', movieDetails)
                     setDetails(movieDetails);
                 } else if (mediaType === 'tv') {
                     const seriesDetails = await tmdbApi.getSeriesDetails(movieId);
@@ -31,17 +33,38 @@ export default function MovieInfo() {
     }
 
     return (
-        <div className="container">
-            <h1>{details.title || details.name}</h1>
-            <img src={`https://image.tmdb.org/t/p/w500${details.poster_path}`} alt={details.title || details.name}/>
-            <p>{details.overview}</p>
-            <p>Release Date: {details.release_date || details.first_air_date}</p>
-            <p>Genres: {details.genres.map(genre => genre.name).join(', ')}</p>
-            <p>Production Companies: {details.production_companies.map(company => company.name).join(', ')}</p>
-            <p>Spoken Languages: {details.spoken_languages.map(language => language.name).join(', ')}</p>
-            <p>Status: {details.status}</p>
-            <p>Vote Average: {details.vote_average}</p>
-            <p>Vote Count: {details.vote_count}</p>
+        <div className="container mx-auto p-4 text-white">
+            <div className="text-center">
+                <h1 className="text-3xl font-bold mb-4">{details.title || details.name}</h1>
+                <img className="block mx-auto" src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
+                     alt={details.title || details.name}/>
+            </div>
+            <div className="border border-gray-300 shadow overflow-hidden sm:rounded-lg mb-4 bg-transparent">
+                <div className="px-4 py-5 sm:px-6 flex justify-between items-center">
+                    <h3 className="text-lg leading-6 font-medium">Movie Details</h3>
+                    {mediaType === 'movie' && (
+                        <button onClick={() => addToGraph(details)} className="btn btn-sm btn-secondary">Add Movie To
+                            Graph</button>
+                    )}
+                </div>
+                <div className="border-t border-gray-200">
+                    <dl>
+                        <p className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">Overview: {details.overview}</p>
+                        <p className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">Release
+                            Date: {details.release_date || details.first_air_date}</p>
+                        <p className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">Genres: {details.genres.map(genre => genre.name).join(', ')}</p>
+                        <p className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">Production
+                            Companies: {details.production_companies.map(company => company.name).join(', ')}</p>
+                        <p className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">Spoken
+                            Languages: {details.spoken_languages.map(language => language.name).join(', ')}</p>
+                        <p className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">Status: {details.status}</p>
+                        <p className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">Vote
+                            Average: {details.vote_average}</p>
+                        <p className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">Vote
+                            Count: {details.vote_count}</p>
+                    </dl>
+                </div>
+            </div>
         </div>
     );
 }
