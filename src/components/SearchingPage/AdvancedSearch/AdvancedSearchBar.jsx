fb9319123f3680f9a1209adf50eb875b0de31b51
StagnantBytes <artyom.shevchenko2005@gmail.com>
2024-05-14 09:48:58 +0200

    Able to add to movies to graph, still need to work out a bit og bugs such as the name of the added movie not shoving in the graph page. IDK why

--- src/components/SearchingPage/AdvancedSearch/AdvancedSearchBar.jsx
+++ src/components/SearchingPage/AdvancedSearch/AdvancedSearchBar.jsx
@@ -1,11 +1,15 @@
 import {useFilterStore} from "../../../services/filterStore.js";
-import FilterList from "../../../assets/data/filters.json";
+import {useState} from "react";
+import movieFilters from "../../../assets/data/movieFilters.json";
+import tvShowFilters from "../../../assets/data/tvShowFilters.json";
 import FilterDropdown from "./DropDownMenu.jsx";
 import FilterCheckbox from "./FilterCheckBox.jsx";
+
 
 export default function AdvancedSearchBar({setSearchQuery}) {
     const addFilter = useFilterStore(state => state.addFilter);
     const filters = useFilterStore(state => state.filters);
+    const [isFiltersShown, setIsFiltersShown] = useState(false);
 
     // Construct search parameters from filters and search query.
     const constructSearchParams = () => {
@@ -31,22 +35,49 @@ export default function AdvancedSearchBar({setSearchQuery}) {
             <div className={"card-body"}>
                 <h2 className={"card-title text-4xl font-bold mb-10 justify-center"}>Advanced Search</h2>
 
-                {/* Render FilterDropdowns for each filter category */}
-                {Object.entries(FilterList.movieFilters).map(([filterCategory, filterValues]) => (
-                    <FilterDropdown
-                        key={filterCategory}
-                        title={filterCategory}
-                        options={filterValues.map(filterValue => (
-                            <FilterCheckbox key={filterValue} filter={filterCategory} value={filterValue}/>
-                        ))}
-                        handleFilterChange={handleFilterChange}
-                    />
-                ))}
+                {/* Button to toggle filters */}
+                <button onClick={() => setIsFiltersShown(!isFiltersShown)}>
+                    {isFiltersShown ? "Hide Filters" : "Show Filters"}
+                </button>
+
+                {/* Render FilterDropdowns for each filter category and the Apply Filters button if filters are shown */}
+                {isFiltersShown && (
+                    <div className="flex justify-between">
+                        <div className="w-full pr-2">
+                            {Object.entries(movieFilters).map(([filterCategory, filterValues]) => (
+                                <FilterDropdown
+                                    key={filterCategory}
+                                    title={filterCategory}
+                                    options={filterValues.map(filterValue => (
+                                        {value: filterValue, label: filterValue} // assuming the label and value are the same
+                                    ))}
+                                    handleFilterChange={handleFilterChange}
+                                />
+                            ))}
+                        </div>
+
+                        <div className="w-full pl-2">
+                            {Object.entries(tvShowFilters).map(([filterCategory, filterValues]) => (
+                                <FilterDropdown
+                                    key={filterCategory}
+                                    title={filterCategory}
+                                    options={filterValues.map(filterValue => (
+                                        {value: filterValue, label: filterValue} // assuming the label and value are the same
+                                    ))}
+                                    handleFilterChange={handleFilterChange}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                )}
 
                 {/* Button to apply filters */}
-                <button className={"btn btn-primary"} onClick={() => setSearchQuery(constructSearchParams())}>Apply
-                    Filters
-                </button>
+                {isFiltersShown && (
+                    <button className={"btn btn-primary"}
+                            onClick={() => setSearchQuery(constructSearchParams())}>Apply
+                        Filters
+                    </button>
+                )}
             </div>
         </div>
     );
---
+++ src/services/AddToGraph.js
@@ -0,0 +1,41 @@
+import {useMovieStore, useMovieConnectionStore} from "./store.js";
+import Movie from './movieClass.js';
+
+const useAddToGraph = () => {
+    const movieStore = useMovieStore.getState();
+    const connectionStore = useMovieConnectionStore.getState();
+
+    return (details) => {
+        console.log('Details for AddToGraph:', details);
+        // Check if details object has an id property
+        if (!details.id) {
+            console.error('Details object does not have an id property:', details);
+            return;
+        }
+
+        // Create a new Movie object
+        const newMovie = new Movie(details);
+
+        movieStore.addMovie(newMovie);
+
+        // Get the first movie in the store
+        const firstMovie = movieStore.movies[0];
+
+        if (firstMovie) {
+            // Check if firstMovie object has an id property
+            if (!firstMovie.movieId) {
+                console.error('First movie object does not have an id property:', firstMovie);
+                return;
+            }
+
+            // Add a connection from the new movie to the first movie
+            connectionStore.addConnection(newMovie.movieId, firstMovie.movieId);
+        }
+
+        // Log the state of the stores
+        console.log(movieStore);
+        console.log(connectionStore);
+    }
+}
+
+export default useAddToGraph;
